c63c786d2e7a925d27a194cc34935439aa5664b7
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2016-02-15 18:15:00 +0900

    Add license file, update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule RandomString.Mixfile do
     [app: :random_string,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description,
+     package: package,
+     deps: deps,
+   ]
   end
 
   # Configuration for the OTP application
