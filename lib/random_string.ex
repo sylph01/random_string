a49d38c2f59eff58bc1d6a36f6503cf57785ad7b
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-10-23 10:20:37 +0900

    Use module variables for pre-defined data structures

--- lib/random_string.ex
+++ lib/random_string.ex
@@ -1,4 +1,19 @@
 defmodule RandomString do
+  @basic_sets %{
+    numeric:   (Enum.to_list 48..57),
+    uppercase: (Enum.to_list 65..90),
+    lowercase: (Enum.to_list 97..122)
+  }
+
+  @compound_sets %{
+    alphabetical: @basic_sets[:uppercase] ++ @basic_sets[:lowercase],
+    alphanumeric: @basic_sets[:uppercase] ++ @basic_sets[:lowercase] ++ @basic_sets[:numeric]
+  }
+
+  @character_sets Map.merge(@basic_sets, @compound_sets)
+
+  @misleading_chars '01258' ++ 'ijlouv' ++ 'BIOSUVZ'
+
   def take(n) when is_integer(n) do
     take(n, :alphanumeric)
   end
@@ -16,7 +31,7 @@ defmodule RandomString do
   end
 
   def stream(character_classes) when is_list(character_classes) do
-    list = Enum.reduce(character_classes, [], fn (x, acc) -> acc ++ character_sets()[x] end)
+    list = Enum.reduce(character_classes, [], fn (x, acc) -> acc ++ @character_sets[x] end)
     Stream.repeatedly(fn -> :lists.nth(:rand.uniform(length(list)), list) end)
   end
 
@@ -25,29 +40,10 @@ defmodule RandomString do
   end
 
   def stream_without_misleading_characters do
-    stream(:alphanumeric) |> Stream.filter(fn x -> !(Enum.member? misleading_chars(), x) end)
+    stream(:alphanumeric) |> Stream.filter(fn x -> !(Enum.member? @misleading_chars, x) end)
   end
 
   def stream_without_characters(character_list) when is_list(character_list) do
     stream(:alphanumeric) |> Stream.filter(fn x -> !(Enum.member? character_list, x) end)
-  end
-
-  defp character_sets do
-    basic_sets = %{
-      numeric:    (Enum.to_list 48..57),
-      uppercase:  (Enum.to_list 65..90),
-      lowercase:  (Enum.to_list 97..122)
-    }
-
-    compound_sets = %{
-      alphabetical: basic_sets[:uppercase] ++ basic_sets[:lowercase],
-      alphanumeric: basic_sets[:uppercase] ++ basic_sets[:lowercase] ++ basic_sets[:numeric]
-    }
-
-    Map.merge(basic_sets, compound_sets)
-  end
-
-  defp misleading_chars do
-    '01258' ++ 'ijlouv' ++ 'BIOSUVZ'
   end
 end
